165976b130ea986c1cdc59f8c6540d22e96f1257
snptkdn <400m.wflick@gmail.com>
2023-01-15 20:00:07 +0900

    WebAPIを叩く際の雛形を追加した。

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use anyhow::Result;
+use reqwest::Client;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    let client = Client::new();
+    let url = "https://zipcloud.ibsnet.co.jp/api/search";
+    let response = client
+        .get(url)
+        .query(&[("zipcode", "1000002")])
+        .send()
+        .await?;
+    let body = response.text().await?;
+    println!("{}", body);
     Ok(())
 }
