66c785174ac5f7a4e98e7cabe1010db9152573e7
snptkdn <400m.wflick@gmail.com>
2023-01-15 21:22:13 +0900

    指定した日付に対してバッテリー残量が低い日時があったか否かを判定する関数を実装した。

--- src/dto/sleep_periods.rs
+++ src/dto/sleep_periods.rs
@@ -1,8 +1,25 @@
 use serde::Deserialize;
+use chrono::{DateTime, Local};
 
 #[derive(Debug, Deserialize)]
 pub struct SleepPeriodsData {
     data: Vec<SleepPeriodData>
+}
+
+impl SleepPeriodsData {
+    pub fn is_low_battery(self, today: &DateTime<Local>) -> bool{
+        self
+        .data
+        .into_iter()
+        .filter(
+            |data: &SleepPeriodData|
+            data.day == today.format("%Y-%m-%d").to_string()
+        )
+        .any(
+            |data: SleepPeriodData|
+            data.low_battery_alert
+        )
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -12,3 +29,5 @@ struct SleepPeriodData {
     day: String
 }
 
+
+// !TODO:test
